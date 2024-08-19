32cf2bf6d7ba39b6b9b75604e7b59ab6462b1e6d
조성준 <39251873+dlddu@users.noreply.github.com>
2024-08-19 10:40:43 +0900

    [Silver IV] Title: 스택, Time: 492 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+stack = []
+for _ in range(N):
+    s = input().split()
+
+    match s[0]:
+        case "push":
+            stack.append(s[1])
+        case "pop":
+            if stack:
+                print(stack.pop())
+            else:
+                print(-1)
+        case "size":
+            print(len(stack))
+        case "empty":
+            if stack:
+                print(0)
+            else:
+                print(1)
+        case "top":
+            if stack:
+                print(stack[-1])
+            else:
+                print(-1)
